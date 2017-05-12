093a12af1ca933bf2e930f212919d26c1af8467f
Gautam Kishore <gautam@eulogik.com>
2017-05-12 23:22:28 +0530

    finished
    also added bootstrap to beautify

--- index.js
+++ index.js
@@ -6,13 +6,26 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var onlineUsers = new Array();
 io.on('connection', function(socket){
-  console.log('a user connected');
+
+  socket.on('people online', function(user){
+    console.log('Connected now: ' + user);
+    onlineUsers.push({"name":user, "since":Date.now(), "socket":socket.id});
+    io.emit('online now', onlineUsers);
+  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
+
+    onlineUsers = onlineUsers.filter(function(el) {
+        return el.socket !== socket.id;
+    });
+    io.emit('online now', onlineUsers);
   });
+
 });
-    
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
